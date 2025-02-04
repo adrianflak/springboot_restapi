fd95eb0f96378e7798a0b53ac58dd16848ec5672
Adrian <kontakt@adrianflak.pl>
2025-02-04 18:06:07 +0100

    new frontend

--- frontend/src/views/UserDetails.vue
+++ frontend/src/views/UserDetails.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
       <h1>Szczegóły użytkownika</h1>
-      <p>ID: {{ userId }}</p>
+      <p>ID: {{ userDetails.id }}</p>
+      <p>Imię: {{ UserDetails.name }}</p>
+      <p>Email: {{ UserDetails.email }}</p>
     </div>
   </template>
   
   <script>
+  import UserDetails from '@/components/UserDetails.vue';
   import { useRoute } from 'vue-router';
   
   export default {
