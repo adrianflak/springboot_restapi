4fedfc3e919987faa7a25aa506f6a36ca03cc2bb
Ubuntu <student@master.nzzhprinmu2erbgljufytqdoee.ex.internal.cloudapp.net>
2025-02-06 16:51:04 +0000

    i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,34 @@ import App from './App.vue'
 import router from './router';
 import { BootstrapVue3, IconsPlugin } from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
+import { createI18n } from 'vue-i18n';
+
+
+const messages = {
+	en: {
+	message: {
+	hello: 'Hello World'
+	}
+},
+	pl: {
+	message: {
+	hello: 'Cześć'
+	}
+}
+};
+
+const i18n = createI18n({
+	locale: 'en',
+	fallbackLocale: 'en',
+	messages,
+}); 
+
 
 const app = createApp(App);
 
-app.use(BootstrapVue3)
-app.use(IconsPlugin)
-
+app.use(BootstrapVue3);
+app.use(IconsPlugin);
 app.use(router);
+app.use(i18n);
 app.mount('#app');
 
